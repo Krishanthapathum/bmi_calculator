56405f1f23ef20c0067280835f1a441e7b72f5fa
Krishanthapathum <kgkppremalal@students.nsbm.ac.lk>
2023-06-25 00:07:10 +0530

    starting ui

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: Center(child: Text("BMI Calculator")),
+      home: Scaffold(
+        body: Center(
+          child: Text("BMI"),
+        ),
+      ),
     );
   }
 }
