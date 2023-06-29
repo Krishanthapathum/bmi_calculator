851e2b7eb570e2f54d80f8d6003695777a7951eb
Krishanthapathum <kgkppremalal@students.nsbm.ac.lk>
2023-06-29 12:05:34 +0530

    Develop Splash Screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:bmi_calculator/screens/spalsh_screen.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -10,11 +11,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text("BMI"),
-        ),
-      ),
+      debugShowCheckedModeBanner: false,
+      home: SplashScreen(),
     );
   }
 }
