8119b7b5551014553d9f5b05df3ccda8df03630a
Krishanthapathum <kgkppremalal@students.nsbm.ac.lk>
2023-06-29 18:37:49 +0530

    develop home screen

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,191 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  TextEditingController heightController = TextEditingController();
+  TextEditingController weightController = TextEditingController();
+
+  double bmi = 0;
+  int myNum = 5;
+
+  @override
+  Widget build(BuildContext context) {
+    // double devhight = MediaQuery.of(context).size.height;
+    double devwidth = MediaQuery.of(context).size.width;
+
+    return Scaffold(
+      appBar: AppBar(
+        leading: const Icon(Icons.person),
+        actions: const [
+          Icon(Icons.logout),
+          SizedBox(width: 10),
+        ],
+        title: const Text(
+          "BMI Calculator",
+          style: TextStyle(
+            fontSize: 25,
+          ),
+        ),
+        centerTitle: true,
+        backgroundColor: const Color.fromRGBO(147, 39, 143, 1),
+      ),
+      body: SizedBox(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      const Text(
+                        "Your BMI:",
+                        style: TextStyle(
+                          fontSize: 20,
+                        ),
+                      ),
+                      const SizedBox(height: 1),
+                      Text(
+                        bmi.toStringAsFixed(2),
+                        style: const TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontFamily: 'Rockwell',
+                          fontSize: 70,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+            const SizedBox(height: 90),
+            Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Column(
+                    children: [
+                      const Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.end,
+                        children: [
+                          Text(
+                            "Height",
+                            style: TextStyle(
+                              fontSize: 25,
+                              fontWeight: FontWeight.w500,
+                            ),
+                          ),
+                          SizedBox(width: 5),
+                          Text(
+                            "(cm)",
+                            style: TextStyle(
+                              fontSize: 15,
+                            ),
+                          ),
+                        ],
+                      ),
+                      const SizedBox(height: 10),
+                      SizedBox(
+                        width: devwidth * 0.4,
+                        child: TextField(
+                          controller: heightController,
+                          keyboardType: TextInputType.number,
+                          decoration: InputDecoration(
+                            filled: true,
+                            fillColor: const Color.fromRGBO(183, 166, 181, 1),
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(15),
+                              borderSide: BorderSide.none,
+                            ),
+                            hintText: 'Enter Height',
+                          ),
+                        ),
+                      )
+                    ],
+                  ),
+                  Column(children: [
+                    const Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.end,
+                      children: [
+                        Text(
+                          "Weight",
+                          style: TextStyle(
+                              fontSize: 25, fontWeight: FontWeight.w500),
+                        ),
+                        SizedBox(width: 5),
+                        Text(
+                          "(kg)",
+                          style: TextStyle(
+                            fontSize: 15,
+                          ),
+                        ),
+                      ],
+                    ),
+                    const SizedBox(height: 10),
+                    SizedBox(
+                      width: devwidth * 0.4,
+                      child: TextField(
+                        controller: weightController,
+                        keyboardType: TextInputType.number,
+                        decoration: InputDecoration(
+                          filled: true,
+                          fillColor: const Color.fromRGBO(183, 166, 181, 1),
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(15),
+                            borderSide: BorderSide.none,
+                          ),
+                          hintText: 'Enter Weight',
+                        ),
+                      ),
+                    )
+                  ]),
+                ]),
+            const SizedBox(height: 60),
+            GestureDetector(
+              onTap: () {
+                double height = double.parse(heightController.text) / 100;
+                double weight = double.parse(weightController.text);
+
+                bmi = weight / (height * height);
+
+                setState(() {
+                  bmi = bmi;
+                });
+              },
+              child: Container(
+                  width: devwidth * 0.8,
+                  height: 60,
+                  decoration: ShapeDecoration(
+                    color: const Color(0xFF93278F),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(35),
+                    ),
+                  ),
+                  child: const Center(
+                    child: Text(
+                      'Calculate',
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 25,
+                        fontWeight: FontWeight.w700,
+                      ),
+                    ),
+                  )),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
